4d865330fd41daab8dea913b095c8da4f93578f0
Julian Rivera <julianarevircoins@gmail.com>
2023-03-01 19:37:31 -0500

    config

--- app.js
+++ app.js
@@ -1,20 +1,43 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var createError = require("http-errors");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require("./routes/users");
+var authRouter = require("./routes/auth");
 
 var app = express();
-
-app.use(logger('dev'));
+app.set("trust proxy", 1);
+app.enable("trust proxy");
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URI], // <== URL of our future React app
+  })
+);
+app.use("/users", usersRouter);
+app.use("/auth", authRouter);
 
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
+  });
 module.exports = app;
